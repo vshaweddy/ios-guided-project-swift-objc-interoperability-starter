deaf127af9187fc4a96c4c74d3181bb89edb63cc
Paul Solt <PaulSolt@gmail.com>
2020-01-27 11:33:41 -0500

    Updated starter project with starting xcode projects

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/ContactsTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ContactsTableViewController.swift
+//  Contacts-Hybrid
+//
+//  Created by Paul Solt on 12/16/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+
+class ContactsTableViewController: UITableViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+	}
+	
+	
+	// MARK: UITableViewDataSource methods
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		
+		
+		return 10
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
+		
+		cell.textLabel?.text = "HI"
+		
+		return cell
+	}
+
+}
