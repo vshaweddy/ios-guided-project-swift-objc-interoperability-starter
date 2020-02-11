d39c419e049c2a9e07af4c68ba382da28c5ed44c
Vici Shaweddy <vshaweddy@gmail.com>
2020-02-10 23:12:05 -0500

    added bridging header

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/LSIContactController.h
@@ -0,0 +1,17 @@
+//
+//  LSIContactController.h
+//  Contacts-Hybrid
+//
+//  Created by Vici Shaweddy on 2/10/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIContactController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/LSIContactController.m
@@ -0,0 +1,13 @@
+//
+//  LSIContactController.m
+//  Contacts-Hybrid
+//
+//  Created by Vici Shaweddy on 2/10/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIContactController.h"
+
+@implementation LSIContactController
+
+@end
